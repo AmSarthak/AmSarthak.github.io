6d1feaa81b402e5a37af158ee88f8ef67bceadbc
Sarthak Chakraborty <sarthak.chakraborty0@gmail.com>
2021-01-02 00:23:45 +0530

    Add files via upload
    Final Commit

---
+++ js/profile.js
@@ -0,0 +1,20 @@
+function goTo(arg){
+    if(arg=='fb'){
+        window.open('https://facebook.com/sarthak.chakraborty3');
+    }
+    if(arg=='mail'){
+        window.open('mailto:sarthak.chakraborty0@gmail.com');
+    }
+    if(arg=='github'){
+        window.open('https://github.com/amsarthak');
+    }
+    if(arg=='linkedin'){
+        window.open('https://www.linkedin.com/in/amsarthak/');
+    }
+}
+
+function downloadResume(){
+    window.open('../css/Resume_new.pdf');
+}
+
+$('.carousel').carousel();
